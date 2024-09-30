1c0baa82f51070eae2e923a563f47f3a7ef74cf8
HAREESH MOHAN S <97726842+hareesh-mohan@users.noreply.github.com>
2024-09-30 15:04:09 +0530

    Update company_signup.jsx

--- src/components/login/company_signup.jsx
+++ src/components/login/company_signup.jsx
@@ -44,12 +44,12 @@ function Company_signup() {
     <form onSubmit={handleSubmit} className='form-container'>
       <h1>Sign Up</h1>
       <label>
-        {/* Name: */}
+        Name:
         <input type="text" value={name} placeholder="Enter company name" onChange={(e) => setName(e.target.value)} />
       </label>
       
       <label>
-        {/* Email: */}
+        Email:
         <input type="email" value={email} placeholder="Enter company email" onChange={(e) => setEmail(e.target.value)} />
       </label>
       
